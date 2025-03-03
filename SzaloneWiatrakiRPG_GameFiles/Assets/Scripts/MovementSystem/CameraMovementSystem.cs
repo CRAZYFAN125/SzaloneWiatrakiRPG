eb07b30f487cbc4404852042476bc07587b54329
CRAZYFAN125 <alan.swietek@gmail.com>
2025-03-04 00:16:30 +0100

    Camera movement system + Edward
    Added ability to move camera
    
    And Edward model

--- SzaloneWiatrakiRPG_GameFiles/Assets/Scripts/MovementSystem/CameraMovementSystem.cs
+++ SzaloneWiatrakiRPG_GameFiles/Assets/Scripts/MovementSystem/CameraMovementSystem.cs
@@ -8,6 +8,12 @@ namespace Assets.Scripts.MovementSystem
         public InputActionReference movement;
         public InputActionReference rotation;
         public InputActionReference zoom;
+        public GameObject PlayerCamera;
+
+        public float moveSpeed = 10f;
+
+        Vector2 movementDir = Vector2.zero;
+        private Transform camTransf;
 
         private void OnEnable()
         {
@@ -23,20 +29,35 @@ namespace Assets.Scripts.MovementSystem
             zoom.action.performed -= ZoomCamera;
         }
 
+        private void Start()
+        {
+            camTransf = PlayerCamera.transform;
+        }
+
+        private void Update()
+        {
+            camTransf.transform.Translate(movementDir.y * Time.deltaTime * moveSpeed * camTransf.forward, Space.World);
+            camTransf.Translate(movementDir.x * Time.deltaTime * moveSpeed * camTransf.right, Space.World);
+            movementDir = Vector2.zero;
+        }
+
         private void MoveCamera(InputAction.CallbackContext context)
         {
-            //Camera Movement Logic
+            movementDir = context.action.ReadValue<Vector2>();
+            movementDir.Normalize();
         }
 
         private void RotateCamera(InputAction.CallbackContext context)
         {
-            //Camera Rotation Logic
+            camTransf.transform.rotation = Quaternion.Euler(new Vector3(0, camTransf.transform.rotation.eulerAngles.y + context.action.ReadValue<float>() * 45, 0));
+            print(camTransf.rotation.eulerAngles);
         }
 
 
         private void ZoomCamera(InputAction.CallbackContext context)
         {
             //Camera Zoom Logic
+            Camera.main.transform.Translate(Camera.main.transform.forward * context.action.ReadValue<float>());
         }
     }
 }
