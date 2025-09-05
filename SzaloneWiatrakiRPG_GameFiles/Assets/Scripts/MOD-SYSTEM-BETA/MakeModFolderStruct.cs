15a944d1a4541fab64ef884500b4acaa1f1edf48
CRAZYFAN125 <alan.swietek@gmail.com>
2025-09-06 01:07:59 +0200

    ModSupport part 2
    First iterations of Mod Loader, basic API, OBJ loading?
    
    Low-Medium End Support?

---
+++ SzaloneWiatrakiRPG_GameFiles/Assets/Scripts/MOD-SYSTEM-BETA/MakeModFolderStruct.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Crazy.ModSystem
+{
+    public class MakeModFolderStruct : MonoBehaviour
+    {
+        public static MakeModFolderStruct Instance;
+        /// <summary>
+        /// Ścieżka do folderu z modami
+        /// </summary>
+        public string ModPath { get; private set; }
+        public void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(this);
+
+            ModPath = Path.Combine(Application.dataPath, "Mods");
+            CheckFolderStruct();
+
+        }
+
+        public bool CheckFolderStruct()
+        {
+
+            if (string.IsNullOrEmpty(ModPath))
+                return false;
+            if (!Directory.Exists(ModPath))
+                Directory.CreateDirectory(ModPath);
+            return true;
+        }
+    }
+}
--- SzaloneWiatrakiRPG_GameFiles/Assets/Scripts/MOD-SYSTEM-BETA/ModLoader.cs
+++ SzaloneWiatrakiRPG_GameFiles/Assets/Scripts/MOD-SYSTEM-BETA/ModLoader.cs
@@ -1,5 +1,7 @@
+using Dummiesman;
 using MoonSharp.Interpreter;
 using System;
+using System.IO;
 using UnityEngine;
 
 namespace Crazy.ModSystem
@@ -7,11 +9,126 @@ namespace Crazy.ModSystem
     public class ModLoader : MonoBehaviour
     {
         private readonly Script script = new();
+        [Multiline(8)]
+        public string scriptString;
+
+        [SerializeField]
+        Material material;
 
         private void Awake()
         {
             Script.DefaultOptions.DebugPrint = s => print(s);
             script.Globals["print"] = (Action<DynValue>)CustomPrint;
+
+            UserData.RegisterType<GameObject>();
+            script.Globals["GetObjectByName"] = (Func<string, GameObject>)GetObjectByName;
+
+            UserData.RegisterType<Transform>();
+            script.Globals["MoveObjectByVector"] = (Func<Transform, Vector3, bool>)MoveObjectByVector;
+
+            UserData.RegisterType<Vector3>();
+            script.Globals["Vector3"] = (Func<float, float, float, Vector3>)((x, y, z) => new Vector3(x, y, z));
+
+            script.Globals["Random"] = (Func<float, float, float>)((min, max) => UnityEngine.Random.Range(min, max));
+
+            UserData.RegisterType<Quaternion>();
+            script.Globals["Quaternion"] = new Table(script);
+            script.Globals.Get("Quaternion").Table["identity"] = Quaternion.identity;
+            script.Globals.Get("Quaternion").Table["Euler"] = (Func<float, float, float, Quaternion>)((x, y, z) => Quaternion.Euler(x, y, z));
+
+            UserData.RegisterType<Texture2D>();
+            UserData.RegisterType<MeshFilter>();
+            UserData.RegisterType<Mesh>();
+
+            script.Globals["RotateObjectByVector"] = (Func<Transform, Vector3, bool>)RotateObjectByVector;
+
+            script.Globals["GetStringFromFile"] = (Func<string, string>)GetStringFromFile;
+            script.Globals["GetTexture2D"] = (Func<string, Texture2D>)GetTexture2D;
+            script.Globals["LoadOBJModel"] = (Func<string, Mesh>)LoadOBJModel;
+            script.Globals["GetComponent"] = (Func<GameObject, string, Component>)GetComponentLUA;
+        }
+        #region Vector Logic
+        private bool MoveObjectByVector(Transform transform, Vector3 position)
+        {
+            if (transform == null) return false;
+
+            transform.position += position;
+            return true;
+        }
+        private bool RotateObjectByVector(Transform transform, Vector3 rotation)
+        {
+            if (transform == null) return false;
+
+            transform.rotation = Quaternion.Euler(rotation + transform.rotation.eulerAngles);
+            return true;
+        }
+        #endregion
+
+        #region File System Logic
+
+        private string GetStringFromFile(string filePath)
+        {
+            var p = Path.Combine(MakeModFolderStruct.Instance.ModPath, filePath);
+
+            if (File.Exists(p))
+            {
+                var data = File.ReadAllText(p);
+                return data;
+            }
+            else
+                return string.Empty;
+        }
+
+        private Texture2D GetTexture2D(string filePath)
+        {
+            var p = Path.Combine(MakeModFolderStruct.Instance.ModPath, filePath);
+            Texture2D texture = new Texture2D(256, 256);
+            if (File.Exists(p))
+            {
+                if (texture.LoadImage(File.ReadAllBytes(p)))
+                    texture.Apply();
+            }
+            else
+            {
+                texture = new(2, 2);
+                texture.SetPixel(1, 1, Color.black);
+                texture.SetPixel(2, 1, Color.magenta);
+                texture.SetPixel(2, 2, Color.black);
+                texture.SetPixel(1, 2, Color.magenta);
+                texture.Apply();
+            }
+
+            material.mainTexture = texture;
+            return texture;
+        }
+
+        private Mesh LoadOBJModel( string path)
+        {
+            var p = Path.Combine(MakeModFolderStruct.Instance.ModPath, path);
+
+            if (File.Exists(p))
+            {
+                OBJLoader loader = new OBJLoader();
+                loader.Load(p);
+                Mesh mesh = new()
+                {
+                    vertices = loader.Vertices.ToArray(),
+                    uv = loader.UVs.ToArray()
+                };
+                return mesh;
+            }
+            print("Emergency Mesh");
+            return new Mesh();
+
+        }
+
+        #endregion
+
+        #region ETC
+
+        private GameObject GetObjectByName(string name)
+        {
+            return GameObject.Find(name);
         }
 
         private void CustomPrint(DynValue value)
@@ -19,14 +136,32 @@ namespace Crazy.ModSystem
             print(value);
         }
 
-        // Start is called before the first frame update
+        private Component GetComponentLUA(GameObject _object, string name)
+        {
+            //! Szansa ¿e siê wyjebie
+            if (_object == null) return null;
+
+            return _object.GetComponent(name);
+        }
+
+        private bool ChangeMesh(GameObject _object, Mesh mesh)
+        {
+            //! Szansa ¿e siê wyjebie
+            if (_object == null) return false;
+            if (mesh == null || mesh == new Mesh()) {
+                Debug.LogWarning("Empty mesh data");
+                return false;
+            }
+            _object.GetComponent<MeshFilter>().mesh = mesh;
+            return true;
+        }
+        #endregion
+
         void Start()
         {
-            script.DoString("print('Hello')");
-            foreach (var item in script.Globals.Keys)
-            {
-                print(item);
-            }
+            script.DoString(scriptString);
+            //foreach (var item in script.Globals.Keys)
+            //    print(item);
         }
     }
 }
